a01833007cf0d00ca36e58c48d94b9510d3bb6d7
Meilky <mikaeljean2k3@gmail.com>
2023-10-24 23:24:31 -0400

    wip

--- vanilla/src/stores/todo.ts
+++ vanilla/src/stores/todo.ts
@@ -4,7 +4,7 @@ import type { Actions } from "../actions/index";
 import type { Todo } from "../models/todo";
 
 export class TodoStore {
-    private _todos: Todo[] = [];
+    private _todos: Map<number, Todo> = new Map();
 
     constructor() {
         DISPATCH.register((data) => {
@@ -13,23 +13,34 @@ export class TodoStore {
     }
 
     public get(): Todo[] {
-        return this._todos;
+        return Array.from(this._todos.values());
     }
 
     private _handleEvents(data: Actions): void {
         switch (data.type) {
-            case "todo.read":
-                this._todos = data.payload;
+            case "todo.created":
+                this._onCreated(data.payload);
                 break;
 
-            case "todo.created":
-                this._todos.push(data.payload);
+            case "todo.read":
+                this._onRead(data.payload);
+                break;
+
+            case "todo.updated":
+                this._onUpdated(data.payload);
                 break;
 
             default:
                 break;
         }
     }
+    private _onCreated(payload: Todo): void {}
+
+    private _onRead(payload: Todo[]): void {}
+
+    private _onUpdated(payload: Todo): void {}
+
+    private _publishEvent(): void {}
 }
 
 export const TODO_STORE = new TodoStore();
